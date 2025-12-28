402bdd3fd9b7a72b79333d6daf0cbbe86daaa5da
binarysimple17 <vred33@gmail.com>
2025-12-28 14:47:39 +0300

    auth moved

--- auth-service/src/main/java/ru/binarysimple/auth/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.binarysimple.auth.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
